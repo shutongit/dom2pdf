066735d2b985e94108df7255d5c112b4944eae9d
shutong <shutong@cretech.com.cn>
2023-11-03 20:59:21 +0800

    删除多余代码

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 
 import App from './App.vue'
-// import MyLoading from '@/components/loading/index.js'
-import LoadingComponent from '@/components/loading/index.vue'
+
 
 const app = createApp(App)
 
